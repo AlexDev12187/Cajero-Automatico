35afec300356069b140b86a59670f874ab4ba206
Alexander <alexanderdiaz2001@gmail.com>
2026-07-31 19:30:08 -0400

    Agrego clase Cliente

---
+++ CajeroAutomatico/src/Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String numeroCuenta;
+    private double saldo;
+    private int pin;
+    private String numRut;
+}
